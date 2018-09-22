179908bdb612ffb4f569befdfe0e3a5a9a5865df
Smushed <smushedcode@gmail.com>
2018-09-22 17:35:44 -0500

    Update the Shell

---
+++ assets/JS/game.js
@@ -0,0 +1,9 @@
+//Initilaze all the characters
+    // Each should have HP, AP, Counter Attack Power
+//Pull the character down from the selector into the fight box
+//Pull the enemy down from the selector into the fight box
+//Attack and increment the player's attack value
+//Counter attack
+
+//Opponent attacks
+//Player counter attacks
